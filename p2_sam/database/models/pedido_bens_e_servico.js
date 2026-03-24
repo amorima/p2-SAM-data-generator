ca4e34462974c04ca591bbb3364ba2a39746c848
Emanuel Gomes <emanuel.jose.f.gomes@gmail.com>
2026-03-24 22:15:15 +0000

    Add models for Pedido_Bens_E_Servico and Bens_E_Servico

---
+++ p2_sam/database/models/pedido_bens_e_servico.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+ const Pedido_Bens_E_Servico = sequelize.define(
+  "pedido_bens_e_servico",
+  {
+   id_pedido: {
+    type: DataTypes.INTEGER,
+    primarykey: true,
+    autoIncrement: true,
+    allowNull: false,
+    unique: true,
+   },
+   tipo_bem_servico: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+   },
+   publico: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+   },
+  },
+  {
+   tableName: "pedido_bens_e_servico",
+   timestamps: false,
+  }
+ );
+
+ Pedido_Bens_E_Servico.associate = (models) => {
+  // Cada doação pertence a um mecenas
+  Pedido_Bens_E_Servico.hasOne(models.Pedido, {
+   foreignKey: "id_pedido",
+   as: "pedido",
+  });
+  Pedido_Bens_E_Servico.hasOne(models.Bens_E_Servico, {
+   foreignKey: "tipo_bem_servico",
+   as: "bens_e_servico",
+  });
+  Pedido_Bens_E_Servico.hasOne(models.Lead, {
+   foreignKey: "id_pedido",
+   as: "lead",
+  });
+ };
+
+ return Pedido_Bens_E_Servico;
+};
