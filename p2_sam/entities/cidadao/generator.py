6b7fc4f5e76ee678d3b7e4b684ba652484039a7b
Emanuel Gomes <emanuel.jose.f.gomes@gmail.com>
2026-04-10 12:32:04 +0100

    feat: testing for implement postal code generation and export functionality

--- p2_sam/entities/cidadao/generator.py
+++ p2_sam/entities/cidadao/generator.py
@@ -1,9 +1,26 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+import math
+import json
+import csv
+import time
+from pathlib import Path
 from faker import Faker
+import pgeocode
 
-faker = Faker('pt_PT')
+# Configuração 
 
-for _ in range(10):
- generatedAddress = faker.address()
- generatedNames = faker.name()
- # print(generatedNames)
-print(generatedAddress)
+faker      = Faker("pt_PT")
+geo        = pgeocode.Nominatim("PT")
+OUTPUT_DIR = Path(__file__).parent.parent / "output"
+N          = 100
+
+
+# cp = faker.postcode()
+# r = cp.replace("-", "")
+
+cp = faker.postcode()
+r  = geo.query_postal_code(cp)
+print(cp)
+print(r)
